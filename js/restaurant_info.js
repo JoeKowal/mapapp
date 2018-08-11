a2aa48c364a8b11fb982be03e8e3ce930d15f817
Joe Kowal <3199871+JoeKowal@users.noreply.github.com>
2018-08-11 16:21:40 -0400

    Alt tag enhancement

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -77,6 +77,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   image.srcset = DBHelper.imageSrcsetForRestaurant(restaurant);
   image.sizes = "(max-width: 350px) 300px, (max-width: 650px) 600px, (min-width: 651px) 800px";
+  image.alt = restaurant.name;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
